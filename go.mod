b346d1dfd119887f618b10527d8a7208dcf6b163
dylantee <aksoonz@gmail.com>
2024-06-22 21:28:02 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module golang-proper
+
+go 1.20
+
+require (
+	cloud.google.com/go v0.115.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	// Define HTTP server.
+	http.HandleFunc("/", helloRunHandler)
+
+	fmt.Println("Starting server at :8080")
+
+	// PORT environment variable is provided by Cloud Run.
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Print("Hello from Cloud Run! The container started successfully and is listening for HTTP requests on $PORT")
+	log.Printf("Listening on port %s", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// helloRunHandler responds to requests by rendering an HTML page.
+func helloRunHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Coming from Proper Golang")
+}
